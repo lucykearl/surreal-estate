d5d1c3dc5bb1f11e1db0097556c3a913eff38933
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-10-05 18:54:26 +0100

    Adding routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import "../styles/app.css";
 import "../styles/NavBar.css";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 import logo from "../components/logo.png";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={Properties} />
+        <Route path="/" element={AddProperty} />
+      </Routes>
       <img className="nav-bar-logo" src={logo} alt="NavBar" />
       <h2>Surreal Estate</h2>
     </div>
