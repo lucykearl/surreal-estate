58b8797c991d5ad66531011e9f323bbb529311c8
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-10-06 19:59:39 +0100

    Fixed 'Vist Properties' route

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "../styles/NavBar.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="nav-bar">
       <ul className="navbar-links">
-        <li className="navbar-links-item">View Properties</li>
-        <li className="navbar-links-item">Add a Property</li>
+        <li className="navbar-links-item">
+          <Link className="navbar-item" to="/view-properties">
+            View Properties
+          </Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link className="navbar-item" to="add-property">
+            Add a Property
+          </Link>
+        </li>
       </ul>
     </div>
   );
